5e83eca7ea5f8738883e528275c7cbad20d3ae39
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-15 11:41:22 +0300

    Task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,9 @@
-// 100-createIteratorObject.js
-
 export default function createIteratorObject(report) {
-  // Use a generator function to iterate through each department and its employees
-  return (function* () {
+  return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
-        yield employee; // Yield each employee
+        yield employee;
       }
     }
-  })();
+  }());
 }

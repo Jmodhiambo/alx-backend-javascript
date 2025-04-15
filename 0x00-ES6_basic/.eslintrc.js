46be64f00e4ab15ac3a53c4ef7c475e56c2d4c96
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-15 08:53:39 +0300

    Task 0

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,35 +1,27 @@
 module.exports = {
   env: {
     browser: false,
-    es6: true,
-    jest: true,
+    es6: true, // Enable ES6 syntax support
+    node: true, // Specify Node.js environment
+    jest: true, // Enable Jest environment
   },
   extends: [
-    'airbnb-base',
-    'plugin:jest/all',
+    'airbnb-base', // Airbnb's base config
+    'plugin:jest/recommended', // Recommended Jest rules
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  plugins: ['jest'], // Include Jest plugin
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2018, // Match your Node.js version (12 supports ECMAScript 2018)
+    sourceType: 'module', // Enable usage of `import`/`export`
   },
-  plugins: ['jest'],
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
+    'no-console': 'off', // Allow console logs
+    'no-shadow': 'off', // Permit variable shadowing
     'no-restricted-syntax': [
       'error',
       'LabeledStatement',
       'WithStatement',
     ],
   },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };
+

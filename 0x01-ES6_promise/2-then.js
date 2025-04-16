fa9337373bbbbbfa5c9da9a893eeff8cf2adfa12
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-16 10:23:00 +0300

    Task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    const status = 200;
-    const body = 'Success';
-
-    return { status, body };
-  })
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
 }

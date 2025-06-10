a7c7671d0d1db1156ef7ac88d5e969214e45c231
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-10 03:08:20 +0300

    Task 0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
+};
